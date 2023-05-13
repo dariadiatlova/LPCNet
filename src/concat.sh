e08fe2f75c072cfbfef82fbf846bddd93ce401c0
dariadiatlova <dari.diatlova@gmail.com>
2023-05-13 08:48:54 +0300

    added arg to concat.sh

--- src/concat.sh
+++ src/concat.sh
@@ -1,6 +1,5 @@
 # Place in 16k-LP7 from TSPSpeech.iso and run to concatenate wave files
 # into one headerless training file
-for i in */*.wav
-do
-sox $i -r 16000 -c 1 -t sw -
+for i in $1/*.wav ; do
+  sox $i -r 16000 -c 1 -t sw -
 done > input.s16
